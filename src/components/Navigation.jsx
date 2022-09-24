d1737e6636b198943e0a639e634a7df45518a28b
Sneha Mahajan <sneh.km@gmail.com>
2022-09-24 16:51:58 -0400

    Style navigation.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,12 +7,15 @@ import './style/Navigation.css';
 function CollapsibleExample() {
   return (
     <Navbar className="navigation" collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Sneha Mahajan</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
           <Nav>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link href="#resume">Resume</Nav.Link>
           </Nav>
         </Navbar.Collapse>
     </Navbar>
