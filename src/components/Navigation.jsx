c55e48a675831298304fff9638138d297ad0fd42
Sneha Mahajan <sneh.km@gmail.com>
2022-09-24 16:36:42 -0400

    Add navigation.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style/Navigation.css';
 
-function Navigation() {
-  return <div>Navigation</div>;
+function CollapsibleExample() {
+  return (
+    <Navbar className="navigation" collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
+          <Nav>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
-export default Navigation;
+export default CollapsibleExample;
