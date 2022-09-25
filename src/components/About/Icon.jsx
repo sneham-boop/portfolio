3cc01284c19156476178aab6f04ff08b4e302019
Sneha Mahajan <sneh.km@gmail.com>
2022-09-24 21:36:19 -0400

    Add about section.

---
+++ src/components/About/Icon.jsx
@@ -0,0 +1,10 @@
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
+function Icon( { icon }) {
+  return (
+    <></>
+  );
+}
+
+export default Icon;
---
+++ src/components/About/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Icon from "./Icon";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
+function About() {
+  return (
+    <Container fluid>
+      <Row>
+        <Col></Col>
+        <Col></Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default About;
