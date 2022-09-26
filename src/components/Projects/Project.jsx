0f47ece62fb22e3f87606affd721920948156b52
Sneha Mahajan <sneh.km@gmail.com>
2022-09-26 11:32:46 -0400

    Add component skeleton for Projects.

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,7 +1,24 @@
-import React from "react"; 
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Stack from "react-bootstrap/Stack";
 
 function Project() {
-  return <div>Project 1</div>;
+  return (
+    <Card className="project">
+      <Card.Img variant="top" src="https://images.pexels.com/photos/3183186/pexels-photo-3183186.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Stack direction="horizontal" gap={3}>
+        <Button variant="primary">Github</Button>
+        <Button variant="primary">Live</Button>
+        </Stack>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default Project;
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,11 +1,26 @@
 import React from "react";
 import Project from "./Project";
+import Container from "react-bootstrap/Container";
+import "./Projects.css";
 
 function Projects() {
+  const projects = () => {
+    return (
+      <>
+        <Project />
+        <Project />
+        <Project />
+        <Project />
+        <Project />
+        <Project />
+      </>
+    );
+  };
   return (
-    <div>
-      <Project />
-    </div>
+    <Container id="projects" fluid>
+      <h3>Projects</h3>
+      <Container id="projects-container">{projects()}</Container>
+    </Container>
   );
 }
 
