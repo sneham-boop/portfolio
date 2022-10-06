0f8c153b3d7160519e68da6033c3266f85aa9e04
Sneha Mahajan <sneh.km@gmail.com>
2022-10-06 11:23:33 -0400

    Drop balls after hovering on them.

--- src/App/components/Jumbotron/Animate.jsx
+++ src/App/components/Jumbotron/Animate.jsx
@@ -1,21 +1,36 @@
 import { gsap } from "gsap";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-export default function Animate(props) {
-  const { color, setColor } = props;
+export default function Animate() {
+  const [mouseLeaveStatus, setMouseLeaveStatus] = useState(false);
+
+  const jumbotronSmallBallsRef = useRef();
+
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      const element = jumbotronSmallBallsRef.current;
+      // gsap.to(element, { zIndex: 20 });
+      mouseLeaveStatus && gsap.to(element, { y: 850, duration: 20 });
+    }, jumbotronSmallBallsRef);
+
+    return () => {
+      ctx.revert();
+    };
+  });
 
   const onEnter = ({ currentTarget }) => {
     gsap.to(currentTarget, { fill: "#fc7a57" });
   };
-  
+
   const onLeave = ({ currentTarget }) => {
+    setMouseLeaveStatus(true);
     gsap.to(currentTarget, { fill: "#ffdd13" });
   };
 
   return (
-    <div className="animate" >
+    <div className="animate" ref={jumbotronSmallBallsRef}>
       <svg>
-        <circle onMouseEnter={onEnter} onMouseLeave={onLeave}/>
+        <circle onMouseEnter={onEnter} onMouseLeave={onLeave} />
       </svg>
     </div>
   );
