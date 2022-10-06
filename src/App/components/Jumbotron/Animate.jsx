2f122881d0faf1d3ea315abbaec41d5eda25278f
Sneha Mahajan <sneh.km@gmail.com>
2022-10-06 13:40:07 -0400

    GSAP is amazing.

--- src/App/components/Jumbotron/Animate.jsx
+++ src/App/components/Jumbotron/Animate.jsx
@@ -10,7 +10,12 @@ export default function Animate() {
     let ctx = gsap.context(() => {
       const element = jumbotronSmallBallsRef.current;
       mouseLeaveStatus &&
-        gsap.to(element, { y: "-1rem", x: "-1rem", ease: "elastic.out(1, 0.3)", duration: 2 });
+        gsap.to(element, {
+          y: "-1rem",
+          x: "-1rem",
+          ease: "elastic.out(1, 0.3)",
+          duration: 2,
+        });
     }, jumbotronSmallBallsRef);
 
     return () => {
@@ -27,12 +32,16 @@ export default function Animate() {
       ease: "elastic.out(1, 0.3)",
       duration: 2,
     });
-    gsap.to(currentTarget, { fill: "#fc7a57" });
+    let tl = gsap.timeline({ repeat: 5, repeatDelay: 1 });
+    tl.to(currentTarget, { fill: "#fc7a57" })
+    .to(currentTarget, { fill: "#fc7a50" })
+    .to(currentTarget, { fill: "#fc7a10", fillOpacity: "0", duration: 1 });
   };
 
   const onLeave = ({ currentTarget }) => {
     setMouseLeaveStatus(true);
     gsap.to(currentTarget, { fill: "#ffdd1300" });
+    // setMouseLeaveStatus(false);
   };
 
   return (
