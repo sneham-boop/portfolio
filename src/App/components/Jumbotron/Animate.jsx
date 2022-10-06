c1e4471964f4101e76fc6cd9a8f2fcf3364b27e0
Sneha Mahajan <sneh.km@gmail.com>
2022-10-06 12:01:03 -0400

    Add elastic movement to balls.

--- src/App/components/Jumbotron/Animate.jsx
+++ src/App/components/Jumbotron/Animate.jsx
@@ -9,8 +9,8 @@ export default function Animate() {
   useEffect(() => {
     let ctx = gsap.context(() => {
       const element = jumbotronSmallBallsRef.current;
-      // gsap.to(element, { zIndex: 20 });
-      mouseLeaveStatus && gsap.to(element, { y: 850, duration: 20 });
+      mouseLeaveStatus &&
+        gsap.to(element, { y: "-1rem", x: "-1rem", ease: "elastic.out(1, 0.3)", duration: 2 });
     }, jumbotronSmallBallsRef);
 
     return () => {
@@ -18,19 +18,33 @@ export default function Animate() {
     };
   });
 
-  const onEnter = ({ currentTarget }) => {
+  const onEnter = (e) => {
+    const element = jumbotronSmallBallsRef.current;
+    const { currentTarget } = e;
+    gsap.to(element, {
+      y: "1rem",
+      x: "1rem",
+      ease: "elastic.out(1, 0.3)",
+      duration: 2,
+    });
     gsap.to(currentTarget, { fill: "#fc7a57" });
   };
 
   const onLeave = ({ currentTarget }) => {
     setMouseLeaveStatus(true);
-    gsap.to(currentTarget, { fill: "#ffdd13" });
+    gsap.to(currentTarget, { fill: "#ffdd1300" });
   };
 
   return (
     <div className="animate" ref={jumbotronSmallBallsRef}>
       <svg>
-        <circle onMouseEnter={onEnter} onMouseLeave={onLeave} />
+        <circle
+          cx="1rem"
+          cy="1rem"
+          r="1rem"
+          onMouseEnter={onEnter}
+          onMouseLeave={onLeave}
+        />
       </svg>
     </div>
   );
