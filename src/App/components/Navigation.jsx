ba64e1da2e0e1258f7fcf8b84ebcc56d0fea0fce
Sneha Mahajan <sneh.km@gmail.com>
2022-10-11 17:31:56 -0400

    Customize nav bar.

--- src/App/components/Navigation.jsx
+++ src/App/components/Navigation.jsx
@@ -1,23 +1,26 @@
 import React from "react";
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './Navigation.scss';
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Navigation.scss";
 
 function CollapsibleExample() {
   return (
-    <Navbar className="navigation" collapseOnSelect expand="lg" variant="dark">
-        <Navbar.Brand href="#home">Sneha Mahajan</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
-          <Nav>
-            <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#skills">Skills</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-            <Nav.Link href="#resume">Resume</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+    <Navbar className="navigation" collapseOnSelect expand="lg">
+      <Navbar.Toggle aria-controls="nav">
+        <span class="material-symbols-rounded">menu</span>
+      </Navbar.Toggle>
+      <Navbar.Collapse className="justify-content-end" id="nav">
+        <Nav>
+          <Nav.Link href="#projects">home</Nav.Link>
+          <Nav.Link href="#projects">projects</Nav.Link>
+          <Nav.Link href="#skills">skills</Nav.Link>
+          <Nav.Link href="#play">play</Nav.Link>
+          <Nav.Link href="#about">about</Nav.Link>
+          <Nav.Link href="#contact">contact</Nav.Link>
+          <Nav.Link href="#resume">resume</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
