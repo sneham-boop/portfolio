d2e3e0b8e3b517b187db5b8f07a2434b500df1e3
Sneha Mahajan <sneh.km@gmail.com>
2022-10-14 14:32:51 -0400

    Fix nav - cursor wasn't working.

--- src/App/components/Home/index.js
+++ src/App/components/Home/index.js
@@ -1,25 +1,23 @@
-import React, {useRef} from "react";
+import React, { useRef } from "react";
 import Jumbotron from "../Jumbotron";
 import Projects from "../Projects";
 import Skills from "../Skills";
 import About from "../About";
 import Contact from "../Contact";
-import Cursor from "../Cursor";
+// import Cursor from "../Cursor";
 // import Resume from "./components/Resume";
 
-function Portfolio() {
-  const app = useRef();
+function Home(props) {
   return (
-    <div ref={app} className="main-container">
-      <Cursor app={app} />
+    <div className="main-container">
       <Jumbotron />
       <Projects />
       <Skills />
       <About />
       <Contact />
-      {/* <Resume /> */}
+
     </div>
   );
 }
 
-export default Portfolio;
+export default Home;
--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,14 @@
-import React, {useRef} from "react";
+import React, { useRef } from "react";
 import "./App.scss";
 import { Outlet } from "react-router-dom";
 import Navigation from "./components/Navigation";
+import Cursor from "./components/Cursor";
 
 function App() {
   const app = useRef();
   return (
     <div ref={app} className="main-container">
+      <Cursor app={app} />
       <Navigation />
       <Outlet />
     </div>
