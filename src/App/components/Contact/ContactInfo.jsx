df12816e4c298c467c9d46e6b5ca9885262e2172
Sneha Mahajan <sneh.km@gmail.com>
2022-12-18 15:27:59 -0500

    Add favicon.

--- src/App/components/Contact/ContactInfo.jsx
+++ src/App/components/Contact/ContactInfo.jsx
@@ -8,7 +8,7 @@ function ContactInfo() {
       <div>
         <InfoRow icon={"call"} info={"(905)-867-8896"} />
         <InfoRow icon={"mail"} info={"hello@snehakmahajan.com"} />
-        <InfoRow icon={"contact_page"} info={"Resume"} />
+        <InfoRow icon={"contact_page"} info={"Resume"} link={"https://www.canva.com/design/DAFIyqWwxtc/wpEDnQ8xAQGGpGvLxjSWNQ/view?utm_content=DAFIyqWwxtc&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"}/>
       </div>
     </section>
   );
--- src/App/components/Contact/InfoRow.jsx
+++ src/App/components/Contact/InfoRow.jsx
@@ -1,7 +1,12 @@
-function InfoRow({ icon, info }) {
+function InfoRow({ icon, info, link }) {
   return (
     <div className="info-row">
-      <span className="material-symbols-rounded">{icon}</span>
+      {link && (
+        <a href={link}>
+          <span className="material-symbols-rounded">{icon}</span>
+        </a>
+      )}
+      {!link && <span className="material-symbols-rounded">{icon}</span>}
       <span> {info}</span>
     </div>
   );
