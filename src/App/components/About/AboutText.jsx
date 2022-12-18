01ea776137487c736379863bed9fceeae4f62bdf
Sneha Mahajan <sneh.km@gmail.com>
2022-12-18 13:01:24 -0500

    Change intro text and darkmode settings.

--- src/App/components/About/AboutText.jsx
+++ src/App/components/About/AboutText.jsx
@@ -20,41 +20,28 @@ function AboutText() {
       <div>
         <h3>About Me</h3>
         <p>
-          Hi, I'm Sneha. I am a full-stack web developer who also enjoys
-          writing, good design and has a weakness for unfrosted vanilla cake. I
-          have a background in industrial automation and electrical engineering.
-          Programming was the one of the parts of my work that made me come
-          alive and I decided it was time to dive into it headfirst. Web
-          development is exciting because there's always so much more to learn.
-          The prospect of building something that someone is going to interact
-          with directly is satisfying and fulfilling.
+          I am a full-stack web developer with a background in electrical
+          engineering and automation. My first taste of web development was when
+          I created a WIX website to sell art a few years ago. The
+          possibility of building something that people would interact with
+          directly was intensely fulfilling. This propelled me into learning to
+          make web applications. Currently, I am exploring CSS animation and
+          understanding the theories behind UI/UX to enable me to become a more
+          efficient web developer.
         </p>
         <p>
-          So far, I've loved exploring new concepts and technologies but the
-          most interesting part has been learning the ways of a different
-          programming language. I am also an artist and learning to code has
-          felt a lot like making art. Anyone can do it, but to do it well you
-          need practice, a lot of repetition and the willingness to experiment
-          and break things. Only then will you truly understand the nature of a
-          medium and with experience, use it to your advantage. I picked
-          Javascript as my medium and its dynamic nature has been fascinating
-          compared to the other strictly typed languages like ladder logic or
-          MATLAB that I've used in the past.
+          As a potential future member of your team, I bring many other things
+          with me, apart from my funky sense of humour and sparkling
+          personality. I enjoy helping people succeed and solving problems.
+          An experience in electrical engineering has toughened my analytical
+          and problem solving skills. It has molded my ability to see the bigger
+          picture when developing applications people will interact with.
+          And...as my colleagues and friends will attest, I am easy to work
+          with and who doesn't appreciate that? 😉
         </p>
         <p>
-          I do not have a clear preference for frontend vs backend development
-          yet. As my projects will show, I enjoy both. However, I drool over
-          good design so I'm a tiny bit partial in that sense.
-        </p>
-        <p>
-          Apart from my funky sense of humour and sparkling personality, I bring
-          many other things with me. I enjoy helping people succeed and solving
-          problems. A 12 year electrical engineering career has strengthened my
-          analytical skills and over time it has molded my ability to see a
-          bigger picture. And...as my colleagues and friends will attest, I am
-          easy to work with, and who doesn't appreciate that? 😉 I'd love to
-          talk to you more about what I'm working on right now and how I could
-          add value to your team.
+          I'd love to talk to you about what I'm currently working on and how I
+          could add value to your team. Contact me below!
         </p>
       </div>
       {showIcons()}
--- src/App/components/Jumbotron/index.js
+++ src/App/components/Jumbotron/index.js
@@ -8,16 +8,28 @@ function Jumbotron() {
 
   return (
     <div className="jumbotron">
-      {Array(607)
+      
+      <section className="intro">
+        <p>Hi, I am Sneha.</p>
+        <h2>Your next creative web developer.</h2>
+      </section>
+
+      <section className="brief-intro">
+        {/* <p>For the past</p> */}
+        <p className="brief-0">In this past year...</p>
+        <p className="brief-1">I've learnt to develop full-stack web applications.</p>
+        <p className="brief-2">Currently, I'm experimenting with CSS animation. </p>
+        <p className="brief-3">And apart, from coding I enjoy music...</p>
+        <p className="brief-4">I enjoy reading books...</p>
+        <p className="brief-5">and making abstract art.</p>
+        <p className="brief-6">Right now, I am looking for my next role...</p>
+        <p className="brief-7">..and I'd like to work with you.</p>
+      </section>
+      {Array(5000)
         .fill(true)
         .map((_, i) => (
           <Animate key={i} color={color} setColor={setColor} />
         ))}
-      <section className="intro">
-        <p>Hello, I am</p>
-        <h2>Sneha Mahajan.</h2>
-        <p>Your next jr. creative web developer.</p>
-      </section>
     </div>
   );
 }
