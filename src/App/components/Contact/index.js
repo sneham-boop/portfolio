17e59e105afcda78eb4fd1e86efa291a9969ddb6
Sneha Mahajan <sneh.km@gmail.com>
2022-12-20 12:04:08 -0500

    Change routing for contact/resume/Navigation

--- src/App/components/Contact/index.js
+++ src/App/components/Contact/index.js
@@ -1,14 +1,30 @@
 import React from "react";
-import ContactInfo from "./ContactInfo";
-import ContactForm from "./ContactForm";
-
+import Button1 from "../Button";
 import "./Contact.scss";
+import { Link } from "react-router-dom";
 
 function Contact() {
   return (
-    <div id="contact" className="contact-row">
-      <ContactInfo />
-      <ContactForm />
+    <div id="contact">
+      <section className="contact-intro">
+        <h2>CONTACT ME</h2>
+        <p>
+          Thank you for taking the time to look through my portfolio. Let's chat
+          about how I can help you in your next project.
+        </p>
+      </section>
+      <section className="contact-info">
+        <div>
+          <span className="contact-email">Email: </span>
+          <span>hello@snehakm.com</span>
+        </div>
+        <Link className="nav-link" to="/resume">
+          <Button1
+            btnText="Resume"
+            custom="about-contact-btn"
+          />
+        </Link>
+      </section>
     </div>
   );
 }
--- src/App/components/Navigation/index.js
+++ src/App/components/Navigation/index.js
@@ -2,24 +2,40 @@ import React from "react";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import DarkMode from "../DarkMode";
+import Logo from "./Logo";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Navigation.scss";
 
 function Navigation() {
+  const handleScroll = () => {
+    const element = document.getElementById("contact");
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+      // element.click({ behavior: "smooth" });
+    }
+  };
   return (
     <Navbar className="navigation" collapseOnSelect expand="sm">
-      <DarkMode/>
+      <Link className="nav-link" to="/home">
+        <Logo />
+      </Link>
+      <DarkMode />
+
       <Navbar.Toggle aria-controls="nav">
         <span className="material-symbols-rounded">menu</span>
       </Navbar.Toggle>
       <Navbar.Collapse className="justify-content-end" id="nav">
         <Nav>
-          <Link className="nav-link" to="/home">
-            Home
-          </Link>
           <Link className="nav-link" to="/resume">
             Resume
+          </Link>
+          <Link
+            className="nav-link"
+            onClick={() => handleScroll()}
+            to="/contact"
+          >
+            Contact
           </Link>
           <Link className="nav-link" to="/play">
             Play
