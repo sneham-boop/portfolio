a2155324c1b57543368002813a145c89c3f4f2f6
Sneha Mahajan <sneh.km@gmail.com>
2022-12-21 13:49:44 -0500

    Make projects section horizontally scrollable.

--- src/App/components/Projects/Project.jsx
+++ src/App/components/Projects/Project.jsx
@@ -1,13 +1,9 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import Button1 from "../Button";
 import More from "./More";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-gsap.registerPlugin(ScrollTrigger);
 
 function Project(props) {
   const { num, name, description, details, github, live, media } = props;
-  const projectNumRef = useRef();
   const liveLink = live || "https://github.com/sneham-boop/colourly";
   const mediaSrc =
     media ||
@@ -18,40 +14,19 @@ function Project(props) {
     window.open(link, "_blank");
   };
 
-  useEffect(() => {
-    let ctx = gsap.context(() => {
-      gsap.registerPlugin();
-      gsap.to(`#project-${num + 1} .project-num `, {
-        scrollTrigger: {
-          trigger: `#project-${num + 1}`,
-          start: "top 50%+=300px",
-          endTrigger: `#project-${num + 1}`,
-          end: "top 50%-=200px",
-          markers: true,
-          scrub: 2,
-          toggleActions: "restart pause reverse pause",
-        },
-        x: -50,
-        duration: 1,
-        opacity: 0.8,
-      });
-    });
-    return () => {
-      ctx.revert();
-    };
-  });
-
   return (
     <>
       <section id={`project-${num + 1}`} className="project">
-        <p className="project-num" ref={projectNumRef}>
-          {num + 1}
-        </p>
         <div className="project-info">
-          <div>
-            <h3>{name}</h3>
-            <p>{description}</p>
-          </div>
+          <h3>
+            {num + 1}. {name}
+          </h3>
+          <img
+            className="project-img"
+            src={mediaSrc}
+            alt="Live project looks like this"
+          />
+          <p>{description}</p>
           <div className="project-buttons">
             <Button1
               btnText="GitHub "
@@ -66,11 +41,6 @@ function Project(props) {
             <More detailDescs={details} name={name} />
           </div>
         </div>
-        <img
-          className="project-img"
-          src={mediaSrc}
-          alt="Live project looks like this"
-        ></img>
       </section>
     </>
   );
--- src/App/components/Projects/index.js
+++ src/App/components/Projects/index.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Project from "./Project";
 import Container from "react-bootstrap/Container";
 import projectsData from "./projectData";
-
 import "./Projects.scss";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function Projects() {
   const projects = () => {
@@ -28,6 +31,30 @@ function Projects() {
       </>
     );
   };
+
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      const projects = gsap.utils.toArray(".project");
+
+      gsap.to(projects, {
+        xPercent: -100 * (projects.length - 1),
+        ease: "none",
+        scrollTrigger: {
+          trigger: "#projects",
+          pin: true,
+          scrub: 1,
+          snap: 1 / (projects.length - 1),
+          end: () => "+=200+=" + document.querySelector("#projects").offsetWidth,
+          markers: true,
+        },
+      });
+    });
+
+    return () => {
+      ctx.revert();
+    };
+  });
+
   return (
     <Container id="projects" fluid>
       <h2>PROJECTS</h2>
