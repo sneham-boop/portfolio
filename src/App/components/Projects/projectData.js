27ec2f72ee05df6171e60666a053c84ee97ff018
Sneha Mahajan <sneh.km@gmail.com>
2023-01-03 18:29:20 -0500

    Update project data for weHike.

--- src/App/components/Projects/projectData.js
+++ src/App/components/Projects/projectData.js
@@ -1,20 +1,20 @@
 const projectsData = {
   0: {
-    name: "weBike",
+    name: "weHike",
     description:
-      "weBike is a single page application where users can find biking events near them or create new biking events for others to join. The goal of this app is to make biking more fun and bring together the biking community by having them join events in an interactive way.",
+      "An app where users can find hiking events near them or create new events for others to join.",
     details: [
-      "weBike is based on my finals project, weRun, for the Lighthouse Labs web development bootcamp. I wanted to add more features to the app and put my own little twist to it! My husband and his close friend, both work at Trek and hearing all the biking stories from them gave me the idea that it will be fun to change over this app to track biking events instead of running events.",
-      "I learnt that clear communication was a key element while working together on the same codebase. Merge conflicts were common and overtime my teammate and I learnt to resolve them more efficiently. Also, working on each others code allowed us to better understand eachothers coding habits and minimize bugs as a result.",
-      "You can log in as one of the test users (email: jane@jane.com, password: password) or create your own account.",
-      "I had to redo a big part of the database and server related code during deployment and next time I will better plan my initial actions to allow for a more seemless deployment experience.",
-      "I would love to add a chat feature for planners and runners to communicate with eachother about event changes.",
+      "weHike is based on was my finals project weRun. My partner Marianne and I finished weRun (link) prior to the demo day and then I ended up adding a few more features to the app and put my own little twist to it! I often go hiking with my dog and wish there was a way to involve other people or plan new hiking routes together.",
+      "Clear communication was critical while working together on the same codebase. Marianne and I often worked on both backend/frontend together and merge conflicts were common. Overtime we learnt to resolve them more efficiently. Also, working on each others code allowed us to better understand our coding habits and minimize bugs.",
+      "This project is live. You can log in as one of the test users (email: jane@jane.com, password: password) or create your own account.",
+      "I had to redo the entire database and a huge part of the server routes during deployment. I will likely select a cloud-based database service right at the start of the project to save time moving forward.",
+      "I would love to add a chat feature for planners and hikers to communicate with eachother about event changes.",
       "I had my friends and colleagues test this app and used their input to improve the apps responsiveness on various devices.",
     ],
-    github: "https://github.com/sneham-boop/werun",
-    live: "https://werun-app.netlify.app",
+    github: "https://github.com/sneham-boop/wehike",
+    live: "https://wehike-app.netlify.app/",
     media:
-      "https://github.com/mariannebourcier/werun/raw/master/docs/images/homepage.png?raw=true",
+      "https://github.com/sneham-boop/wehike/raw/main/docs/images/Home.png",
   },
   3: {
     name: "Colourly",
