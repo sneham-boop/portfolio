f60b8c1fa041472fa73d1246b00329ee14921993
Sneha Mahajan <sneh.km@gmail.com>
2023-01-05 16:48:53 -0500

    Add wild route catching.

--- src/Router.jsx
+++ src/Router.jsx
@@ -14,6 +14,7 @@ import {
 export const Router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
+      <Route path="*" element={<Home/>} />
       <Route index element={<Home />} />
       <Route path="home" element={<Home />}>
         <Route path="contact" element={<Contact />}/>
