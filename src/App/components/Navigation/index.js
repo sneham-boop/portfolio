51aa884c68cdb3f9f3d9cccddd15c54805845767
Sneha Mahajan <sneh.km@gmail.com>
2023-01-05 22:03:09 -0500

    Try another link style.

--- src/App/components/Navigation/index.js
+++ src/App/components/Navigation/index.js
@@ -55,7 +55,7 @@ function Navigation() {
         </Navbar.Toggle>
         <Navbar.Collapse className="justify-content-end" id="nav">
           <Nav>
-            <Link className={linkStyle} to="/resume">
+            <Link className={linkStyle} to="resume">
               {/* <a className={linkStyle} href="/resume" target="_blank" rel="noreferrer"> */}
                 Resume
               {/* </a> */}
