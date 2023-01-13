aa3abd9fdedd9ff1d13160bcbd700d9fb57dfd83
Sneha Mahajan <sneh.km@gmail.com>
2023-01-13 12:13:31 -0500

    Height of Jumbotron - adjsuted

--- src/App/components/About/Section3.jsx
+++ src/App/components/About/Section3.jsx
@@ -1,25 +1,12 @@
 // import Button1 from "../Button";
 
 function Section3() {
-  // const handleScroll = () => {
-  //   const element = document.getElementById("contact");
-  //   if (element) {
-  //     element.scrollIntoView({ behavior: "smooth" });
-  //   }
-  // };
   return (
     <div className="about-section section-3">
       <p>
         Currently I'm looking for my next role. I'd love to talk to you more
         about the problem you are trying to solve and how I could help.
       </p>
-      {/* <div>
-        <Button1
-          btnText="Contact Me"
-          onClick={() => handleScroll()}
-          custom="about-contact-btn"
-        />
-      </div> */}
     </div>
   );
 }
